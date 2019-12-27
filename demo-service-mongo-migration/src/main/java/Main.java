7d08b0d0df8767696bc15d596767071112fc384a
yangjq1 <fzu_yangjinqi@163.com>
2019-12-27 11:44:13 +0800

    demo-service-mongo-migration

---
+++ demo-service-mongo-migration/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import com.mongodb.client.model.Indexes;
+import core.framework.mongo.MongoMigration;
+import org.bson.Document;
+
+/**
+ * @author Else
+ */
+public class Main {
+    public static void main(String[] args) {
+        var migration = new MongoMigration("sys.properties", "sys.mongo.adminURI");
+        migration.migrate(mongo -> {
+            mongo.runCommand(new Document().append("setParameter", 1).append("notablescan", 1));
+        });
+
+        migration = new MongoMigration("sys.properties");
+        migration.migrate(mongo -> {
+            mongo.createIndex("product", Indexes.ascending("name"));
+        });
+    }
+}
