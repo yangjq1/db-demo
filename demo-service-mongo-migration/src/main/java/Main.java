ae81895c1d04e9b77e74ef157f963251999892ef
yangjq1 <fzu_yangjinqi@163.com>
2020-01-02 17:15:53 +0800

    mongo create index

--- demo-service-mongo-migration/src/main/java/Main.java
+++ demo-service-mongo-migration/src/main/java/Main.java
@@ -1,14 +1,8 @@
 import com.mongodb.client.model.Indexes;
 import core.framework.mongo.MongoMigration;
-import core.framework.util.Lists;
 import org.bson.BsonDocument;
 import org.bson.BsonInt32;
 import org.bson.Document;
-import org.bson.conversions.Bson;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 
 /**
  * @author Else
@@ -17,20 +11,17 @@ public class Main {
     public static void main(String[] args) {
         var migration = new MongoMigration("sys.properties", "sys.mongo.adminURI");
         //禁止表扫描
-        migration.migrate(mongo -> {
-            mongo.runCommand(new Document().append("setParameter", 1).append("notablescan", 1));
-        });
+        migration.migrate(mongo ->
+            mongo.runCommand(new Document().append("setParameter", 1).append("notablescan", 1))
+        );
 
         migration = new MongoMigration("sys.properties");
-        //创建索引
-        migration.migrate(mongo -> {
-            mongo.createIndex("products", Indexes.ascending("created_time"));
-        });
-
-        //创建索引
-        migration.migrate(mongo -> {
+        migration.migrate( mongo ->
+            mongo.createIndex("products", Indexes.ascending("created_time"))
+        );
+        migration.migrate(mongo ->
             mongo.createIndex("products", Indexes.compoundIndex(new BsonDocument("name",new BsonInt32(1)),
-                new BsonDocument( "desc",new BsonInt32(1))));
-        });
+                new BsonDocument( "desc",new BsonInt32(1))))
+        );
     }
 }
